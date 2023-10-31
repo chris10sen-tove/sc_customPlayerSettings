6ce1eccb2789bbf0bec7050ce4abe9167f2d4b4d
chris10sen-tove <149208025+chris10sen-tove@users.noreply.github.com>
2023-10-31 11:44:30 -0400

    Update sc_customPlayerSettings.js

--- sc_customPlayerSettings.js
+++ sc_customPlayerSettings.js
@@ -1,66 +1,46 @@
-videojs.registerPlugin('customSettingsButton', function() {
-  const player = this;
-  const settingsButton = document.createElement("button");
-  settingsButton.className = "vjs-control vjs-button vjs-settings-button";
-  settingsButton.innerHTML = "<span class='cog-icon'></span> Settings";
+videojs.registerPlugin('combineSettingsButton', function () {
+  var vPlayer = this;
+  var settingsButton = document.createElement("div");
+  settingsButton.className = "vjs-settings-button vjs-menu-button vjs-menu-button-popup vjs-control vjs-button";
+  settingsButton.innerHTML = "<button class='vjs-settings-button vjs-menu-button vjs-menu-button-popup vjs-button' type='button' aria-disabled='false' title='Settings' aria-haspopup='true' aria-expanded='false'><span class='vjs-icon-placeholder' aria-hidden='true'><i class='fa fa-cog'></i></span><span class='vjs-control-text' aria-live='polite'>Settings</span></button>";
 
-  // Create the settings menu
-  const settingsMenu = document.createElement("div");
-  settingsMenu.className = "settings-menu";
-  settingsMenu.style.display = "none";
-  
-  const captionsOption = document.createElement("div");
-  captionsOption.className = "menu-item";
-  captionsOption.innerText = "Captions";
-  
-  const audioTrackOption = document.createElement("div");
-  audioTrackOption.className = "menu-item";
-  audioTrackOption.innerText = "Audio Track";
-  
-  settingsMenu.appendChild(captionsOption);
-  settingsMenu.appendChild(audioTrackOption);
+  var captionsButton = vPlayer.$(".vjs-subs-caps-button");
+  var audioButton = vPlayer.$(".vjs-audio-button");
 
-  // Insert the button and menu into the control bar
-  const controlBar = player.$(".vjs-control-bar");
-  const insertBeforeNode = player.$(".vjs-volume-panel");
+  if (captionsButton) {
+    captionsButton.style.display = "none";
+  }
+  if (audioButton) {
+    audioButton.style.display = "none";
+  }
+
+  var controlBar = vPlayer.$(".vjs-control-bar");
+  var insertBeforeNode = vPlayer.$(".vjs-volume-panel");
 
   controlBar.insertBefore(settingsButton, insertBeforeNode);
-  controlBar.appendChild(settingsMenu);
 
-  settingsButton.addEventListener("click", function() {
-    settingsMenu.style.display = settingsMenu.style.display === "block" ? "none" : "block";
-  });
+  var settingsMenu = document.createElement("div");
+  settingsMenu.className = "vjs-menu";
+  settingsMenu.innerHTML = "<ul class='vjs-menu-content' role='menu'>" +
+    "<li class='vjs-menu-item vjs-texttrack-settings' tabindex='-1' role='menuitem' aria-disabled='false'>" +
+    "<span class='vjs-menu-item-text'>captions settings</span>" +
+    "<span class='vjs-control-text' aria-live='polite'>, opens captions settings dialog</span></li>" +
+    "<li class='vjs-menu-item vjs-selected' tabindex='-1' role='menuitemradio' aria-disabled='false' aria-checked='true'>" +
+    "<span class='vjs-menu-item-text'>captions off</span>" +
+    "<span class='vjs-control-text' aria-live='polite'>, selected</span></li>" +
+    "<li class='vjs-menu-item vjs-subtitles-menu-item' tabindex='-1' role='menuitemradio' aria-disabled='false' aria-checked='false'>" +
+    "<span class='vjs-menu-item-text'>English</span>" +
+    "<span class='vjs-control-text' aria-live='polite'></span></li>" +
+    "<li class='vjs-menu-item vjs-main-menu-item' tabindex='-1' role='menuitemradio' aria-disabled='false' aria-checked='true'>" +
+    "<span class='vjs-menu-item-text'>en (Main)</span>" +
+    "<span class='vjs-control-text' aria-live='polite'>, selected</span></li>" +
+    "<li class='vjs-menu-item vjs-alternative-menu-item' tabindex='-1' role='menuitemradio' aria-disabled='false' aria-checked='false'>" +
+    "<span class='vjs-menu-item-text'>en (Descriptive)</span>" +
+    "<span class='vjs-control-text' aria-live='polite'></span></li></ul>";
 
-  // Logic for handling captions
-  captionsOption.addEventListener("click", function() {
-    const captionsMenu = player.$(".vjs-menu.vjs-lock-showing");
-    const captionsOffItem = captionsMenu.querySelector(".vjs-menu-item.vjs-selected");
-    const captionsLanguageItem = captionsMenu.querySelector(".vjs-menu-item.vjs-subtitles-menu-item");
+  settingsButton.appendChild(settingsMenu);
 
-    // Handle captions based on their states
-    if (captionsOffItem.getAttribute("aria-checked") === "true") {
-      // Captions are currently off, you can turn them on or switch to a specific language
-      console.log("Captions are currently off.");
-    } else if (captionsLanguageItem.getAttribute("aria-checked") === "true") {
-      // A specific language is selected
-      const language = captionsLanguageItem.querySelector(".vjs-menu-item-text").innerText;
-      console.log("Selected captions language: " + language);
-    }
-  });
-
-  // Logic for handling audio tracks
-  audioTrackOption.addEventListener("click", function() {
-    const audioTracksMenu = player.$(".vjs-menu");
-    const mainAudioTrackItem = audioTracksMenu.querySelector(".vjs-menu-item.vjs-main-menu-item");
-    const descriptiveAudioTrackItem = audioTracksMenu.querySelector(".vjs-menu-item.vjs-alternative-menu-item");
-
-    // Handle audio tracks based on their states
-    if (mainAudioTrackItem.getAttribute("aria-checked") === "true") {
-      // Main audio track is selected
-      console.log("Main audio track is selected.");
-    } else if (descriptiveAudioTrackItem.getAttribute("aria-checked") === "true") {
-      // Descriptive audio track is selected
-      console.log("Descriptive audio track is selected.");
-    }
+  settingsButton.addEventListener("click", function () {
+    settingsMenu.style.display = (settingsMenu.style.display === 'block') ? 'none' : 'block';
   });
 });
